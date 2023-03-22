6bb62bc6f1a1a774d655f5e5ba01c30fb30d2fea
Louis Parent <contact@louis-parent.fr>
2023-03-22 17:51:28 +0100

    Adding initialisation

--- scripts/jsStudio.js
+++ scripts/jsStudio.js
@@ -1,6 +1,31 @@
 class jsStudio
 {
+	constructor(options)
+	{
+		this.setColors(options?.colors);
+		this.setTexts(options?.texts);
+		this.setComponents(options?.components);
+	}
 	
+	setColors(colors)
+	{
+		this.colors = this.colors || new Object();
+		Object.assign(this.colors, colors);
+	}
+	
+	setTexts(texts)
+	{
+		this.texts = this.texts || new Object();
+		this.texts.title = texts?.title || this.texts.title;
+		this.texts.subtitle = texts?.subtitle || this.texts.subtitle;
+		this.texts.content = texts?.content || this.texts.content;
+	}
+	
+	setComponents(components)
+	{
+		this.components = this.components || new Object();
+		Object.assign(this.components, components);
+	}
 };
 
 export default jsStudio;
